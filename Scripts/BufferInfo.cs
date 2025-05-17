b3f7103e98ab30742005a591138cddaa508209aa
Aelstraz <Jakegwizdak@gmail.com>
2025-05-17 17:03:14 +0930

    Added main files

---
+++ Scripts/BufferInfo.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace GPUComputeModule
+{
+    /// <summary>
+    /// A struct to hold information about a compute buffer
+    /// </summary>
+    public struct BufferInfo
+    {
+        public string name;
+        public ComputeBuffer buffer;
+        public Type dataType;
+
+        /// <summary>
+        /// Constructor for BufferInfo
+        /// </summary>
+        /// <param name="name">The name of the buffer</param>
+        /// <param name="buffer">The compute buffer</param>
+        /// <param name="dataType">The data type of the buffer</param>
+        public BufferInfo(string name, ComputeBuffer buffer, Type dataType)
+        {
+            this.name = name;
+            this.buffer = buffer;
+            this.dataType = dataType;
+        }
+    }
+}
