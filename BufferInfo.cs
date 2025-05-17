cde14e4cde464010f89487c3efd9883436e8099d
Aelstraz <Jakegwizdak@gmail.com>
2025-05-17 18:11:22 +0930

    Test structure

--- BufferInfo.cs
+++ BufferInfo.cs
@@ -1,28 +1,25 @@
 using System;
 using UnityEngine;
 
-namespace GPUComputeModule
+/// <summary>
+/// A struct to hold information about a compute buffer
+/// </summary>
+public struct BufferInfo
 {
-    /// <summary>
-    /// A struct to hold information about a compute buffer
-    /// </summary>
-    public struct BufferInfo
-    {
-        public string name;
-        public ComputeBuffer buffer;
-        public Type dataType;
+    public string name;
+    public ComputeBuffer buffer;
+    public Type dataType;
 
-        /// <summary>
-        /// Constructor for BufferInfo
-        /// </summary>
-        /// <param name="name">The name of the buffer</param>
-        /// <param name="buffer">The compute buffer</param>
-        /// <param name="dataType">The data type of the buffer</param>
-        public BufferInfo(string name, ComputeBuffer buffer, Type dataType)
-        {
-            this.name = name;
-            this.buffer = buffer;
-            this.dataType = dataType;
-        }
+    /// <summary>
+    /// Constructor for BufferInfo
+    /// </summary>
+    /// <param name="name">The name of the buffer</param>
+    /// <param name="buffer">The compute buffer</param>
+    /// <param name="dataType">The data type of the buffer</param>
+    public BufferInfo(string name, ComputeBuffer buffer, Type dataType)
+    {
+        this.name = name;
+        this.buffer = buffer;
+        this.dataType = dataType;
     }
 }
